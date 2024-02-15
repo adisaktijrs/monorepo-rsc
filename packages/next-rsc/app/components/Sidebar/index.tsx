bad62653bde13d4d7faf38f69164e5f94e1ce44a
adisaktijrs <adhisakti47@gmail.com>
2024-02-15 22:13:08 +0700

    feat: add base components

---
+++ packages/next-rsc/app/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="m-6 border p-6">
+      <p>lol</p>
+      <p>lol</p>
+      <p>lol</p>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ packages/next-rsc/app/loading.tsx
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <p className="m-6">loading...</p>;
+};
+
+export default loading;
